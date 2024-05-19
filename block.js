eef34df2e4e644c38c0b90f2b6b18eebae5a2dc0
saurabh9921 <saurabhjadhav9921@gmail.com>
2024-05-19 14:46:44 +0530

    creat simple block

---
+++ block.js
@@ -0,0 +1,38 @@
+const {GENESIS_DATA}=require("./config");
+const cryptoHash=require("./crypto-hash");
+
+class Block{
+    constructor({timestamp,prevhash,hash,data}){
+        this.timestamp=timestamp;
+        this.prevhash=prevhash;
+        this.hash=hash;
+        this.data=data; 
+
+    }
+    static genesis(){
+        return new this(GENESIS_DATA);
+    }
+    static minBlock({prevBlock,data}){
+        const timestamp=Date.now();
+        const prevhash=prevBlock.hash;
+
+        return new this({
+        timestamp,
+        data,
+        prevhash,
+        hash:cryptoHash(timestamp,prevhash,data,),
+        });
+
+    }
+}
+ const block1 =new Block({
+     timestamp:"02/09/22",
+     prevhash:"0jjbg",
+     data:"hello w",
+     hash:"0xsaf",});
+ //console.log(block1);
+ const genesisBlock=Block.genesis();
+ console.log(genesisBlock);
+
+ const result=Block.minBlock({prevBlock:block1,data:"block2"});
+ console.log(result);
---
+++ config.js
@@ -0,0 +1,7 @@
+const GENESIS_DATA={
+   timestamp:1,
+   prevHash:'0x000',
+   hash:'0x0123', 
+   data:[]
+}
+module.exports={GENESIS_DATA};
